b43b7902cf6a2fff21f0dd24180453e8039eaf9c
Simonm121 <simonmillerhome@yahoo.com>
2026-05-06 06:39:41 +0100

    Update next.config.js

--- quick-privacy-tools/next.config.js
+++ quick-privacy-tools/next.config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "quickprivacytools.com",
+          },
+        ],
+        destination: "https://www.quickprivacytools.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
